9c746b0cb444f7a19361eecb3ac88d2769d15dd3
hamedi <hamedebrahimi128@gmail.com>
2024-02-19 23:37:25 +0330

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    pocketbaseUsername: "fgsharifdini@gmail.com",
+    pocketbasePassword: "0020396198",
+    pocketbaseUrl: "https://fgsharifdini.pockethost.io/",
+    expirationTime: "3600",
+    sessionCookieKey: "session-key",
+  },
+};
+
+export default nextConfig;
---
+++ providers/guard-provider.tsx
@@ -0,0 +1,23 @@
+import { authorization } from "@/pocketbase/users";
+import { redirect } from "next/navigation";
+
+export const GuardProvider: React.FC<
+  IHasChild & { roleMode: "admin" | "user" }
+> = async ({ children, roleMode }) => {
+  const result = await authorization();
+  console.log(result);
+  
+  if (!!result.error) {
+    redirect("/login");
+  }
+
+  if (result.data?.isAdmin && roleMode === "user") {
+    redirect("/login");
+  }
+
+  if (!result.data?.isAdmin && roleMode === "admin") {
+    redirect("/login");
+  }
+
+  return <>{children}</>;
+};
